31ca5c835b74ae63b1c455c0bb4ad05c2551d980
Vasanth <chinthamvasanth@gmail.com>
2025-06-25 08:16:53 +0530

    Boolean

---
+++ Class/src/main/java/org/example/Boolean.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Boolean {
+    public static void main (String[]args){
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+        int x = 9;
+        int y = 10;
+        int myAge = 25;
+        int votingAge = 18;
+        System.out.println(x > y);
+        System.out.println(x == y);
+        System.out.println(x < y);
+        System.out.println(myAge >= votingAge);
+        System.out.println(myAge <= votingAge);
+    }
+}
