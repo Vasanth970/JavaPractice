320ac25a539e19c51041b8cb9cf47c009c717a77
Vasanth <chinthamvasanth@gmail.com>
2025-06-26 16:53:05 +0530

    Switch

---
+++ Class/src/main/java/org/example/Switch.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Switch {
+    public static void main(String[] args) {
+
+        int day = 5;
+        switch (day) {
+
+            case 1:
+                System.out.println("Day1");
+                break;
+            case 2:
+                System.out.println("Day2");
+                break;
+            case 3:
+                System.out.println("Day3");
+                break;
+            default:
+                System.out.println("No Holiday");
+
+        }
+    }
+}
+
+
