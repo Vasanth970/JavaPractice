41b7b2f0d6e2f50fd1d7fd029cde8d4f3a877c2a
Vasanth <chinthamvasanth@gmail.com>
2025-06-27 22:35:23 +0530

    loops

---
+++ Class/src/main/java/org/example/DoWhile.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class DoWhile {
+    public static void main(String[] args) {
+       int i = 10;
+               do{
+                   System.out.println(i);
+                   i ++;
+               } while (i <  5);
+
+               //the variable i starts at 10, so the condition i < 5 is false immediately - yet the do/while loop still runs once:
+    }
+}
---
+++ Class/src/main/java/org/example/While_Loop.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class While_Loop {
+    public static void main (String[]args){
+        int countdown = 3;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy New Year!!");
+    }
+}
+
